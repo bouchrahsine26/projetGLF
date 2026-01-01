2dc6b8940273baa23022d2c38e6c5213dea91111
Bouchra Hsine <hsinebouchra3@gmail.com>
2026-01-01 20:47:12 +0100

    Initial

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
 /* =========================
    Connexion MongoDB
 ========================= */
-const dbURL = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/hospital";
+const dbURL = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/neohealth";
 
 mongoose
   .connect(dbURL)
